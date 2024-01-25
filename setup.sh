ecd07626879f899f178cf458d64aadb9f7317878
Subie Patel <subie@lightmatter.co>
2024-01-25 22:59:11 +0000

    corrections from running

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# TODO: make these steps idempotent for rerunning after errors.
-
 # TODO: move these into an env file.
 POWERLEVEL10K_INSTALL_PATH=~/powerlevel10k
 FZF_INSTALL_PATH=~/.fzf
@@ -9,23 +7,22 @@ PYENV_INSTALL_PATH=~/.pyenv
 PYENV_SETUP_PYTHON_VERSION=3.9.16
 
 # Get my dotfiles.
-git clone https://github.com/subie/dotfiles.git ~/.dotfiles
+[[ ! -d ~/.dotfiles ]] && git clone https://github.com/subie/dotfiles.git ~/.dotfiles
 
 # symlink dotfiles.
 sudo apt-get -y install stow
-stow --dir=~/.dotfiles --target=~/
+(cd .dotfiles/ && stow .)
 
 # Setup zsh.
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${POWERLEVEL10K_INSTALL_PATH}
+sudo apt-get -y install zsh
+[[ ! -d ${POWERLEVEL10K_INSTALL_PATH} ]] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${POWERLEVEL10K_INSTALL_PATH}
 chsh -s $(which zsh)
-git clone --depth 1 https://github.com/junegunn/fzf.git ${FZF_INSTALL_PATH}
-${FZF_INSTALL_PATH}/install
+[[ ! -d ${FZF_INSTALL_PATH} ]] && git clone --depth 1 https://github.com/junegunn/fzf.git ${FZF_INSTALL_PATH}
+[[ ! -f ~/.fzf.zsh ]] && ${FZF_INSTALL_PATH}/install
 
 # Install pyenv and recentish python version.
-git clone https://github.com/pyenv/pyenv.git ${PYENV_INSTALL_PATH}
-eval "$(${PYENV_INSTALL_PATH}/bin/pyenv init --path)"
+sudo apt-get -y install libssl-dev
+[[ ! -d ${PYENV_INSTALL_PATH} ]] && git clone https://github.com/pyenv/pyenv.git ${PYENV_INSTALL_PATH}
+eval "$(${PYENV_INSTALL_PATH}/bin/pyenv init)"
 ${PYENV_INSTALL_PATH}/bin/pyenv install ${PYENV_SETUP_PYTHON_VERSION}
-${PYENV_INSTALL_PATH}/bin/pyenv shell ${PYENV_SETUP_PYTHON_VERSION}
-pip install powerline-status
-
-# TODO: install emacs
+${PYENV_INSTALL_PATH}/versions/3.9.16/bin/pip install powerline-status
