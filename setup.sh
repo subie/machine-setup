d7a5192ece6e7489bccc6c75e42c1e7a9d57f540
subie <subhash.patel@gmail.com>
2025-03-12 10:51:08 -0700

    remove pyenv

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 # TODO: move these into an env file.
 POWERLEVEL10K_INSTALL_PATH=~/powerlevel10k
 FZF_INSTALL_PATH=~/.fzf
-PYENV_INSTALL_PATH=~/.pyenv
-PYENV_SETUP_PYTHON_VERSION=3.10.12
 
 # Get my dotfiles.
 [[ ! -d ~/.dotfiles ]] && git clone https://github.com/subie/dotfiles.git ~/.dotfiles
@@ -21,13 +19,6 @@ sudo apt-get -y install zsh
 chsh -s $(which zsh)
 [[ ! -d ${FZF_INSTALL_PATH} ]] && git clone --depth 1 https://github.com/junegunn/fzf.git ${FZF_INSTALL_PATH}
 [[ ! -f ~/.fzf.zsh ]] && ${FZF_INSTALL_PATH}/install
-
-# Install pyenv and recentish python version.
-sudo apt-get -y install libssl-dev libbz2-dev libsqlite3-dev libreadline-dev libncurses-dev
-[[ ! -d ${PYENV_INSTALL_PATH} ]] && git clone https://github.com/pyenv/pyenv.git ${PYENV_INSTALL_PATH}
-eval "$(${PYENV_INSTALL_PATH}/bin/pyenv init)"
-${PYENV_INSTALL_PATH}/bin/pyenv install ${PYENV_SETUP_PYTHON_VERSION}
-${PYENV_INSTALL_PATH}/versions/${PYENV_SETUP_PYTHON_VERSION}/bin/pip install powerline-status
 
 # tmux stuff.
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
